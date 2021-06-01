6ec7c7a37f184759e8b8dcbb36897c1d5f345e9e
lamtran <tranvulam.dev@gmail.com>
2021-06-01 23:18:19 +0700

    add morgan

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 const port = process.env.PORT || 5000
 
+// uses
+app.use(morgan('combined'))
+
+// methods
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`My app listening at http://localhost:${port}`)
 })
