999d1ae1b433dabb896b09f36a9b2b22e804d0bd
lamtran <tranvulam.dev@gmail.com>
2021-06-04 22:12:26 +0700

    Refactor code: create routes app/controllers

--- src/index.js
+++ src/index.js
@@ -6,26 +6,28 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 5000
 
+const route = require('./routes')
+
 // template engine
-app.engine('hbs', handlebars({
+app.engine('hbs', handlebars({  // đổi đuôi file handlebars thành hbs
   extname: '.hbs'
 }));
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views')) //layoutsDir
 
+// __dirname gần là path của index.js và tùy biến từ từ (hình như __dirname cũng ảnh hưởng đến nhúng file css trong main.hbs)
 app.use(express.static(path.join(__dirname, 'public')))
 
-// HTTP logger
+//middleware 2 cái dưới hỗ trợ ghi dữ liệu form data query
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+// HTTP logger hiện chi tiết các request trong terminal
 app.use(morgan('combined'))
 
-// methods
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+// routes initial
+route(app);
 
 app.listen(port, () => {
   console.log(`My app listening at http://localhost:${port}`)
---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+const newsRouter = require('./news')
+const siteRouter = require('./site')
+
+function route(app) {
+  app.use('/news', newsRouter)
+  app.use('/', siteRouter)
+
+  // submit form với post thì query không hiện trên url
+  // get: => req.query, post: => req.body (form data)
+}
+
+module.exports = route;
