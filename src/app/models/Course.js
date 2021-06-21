b01ea58998682d2e3727ecc52f2bc70f7d18daca
lamtran <tranvulam.dev@gmail.com>
2021-06-21 22:06:09 +0700

    almost finished

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose')
 const slug = require('mongoose-slug-generator')
 const mongooseDelete = require('mongoose-delete')
+const AutoIncrement = require('mongoose-sequence')(mongoose)
 
 const Schema = mongoose.Schema
 
 const Course = new Schema(
     {
+        _id: { type: Number },
         name: { type: String, require: true },
         description: { type: String },
         image: { type: String },
@@ -13,11 +15,19 @@ const Course = new Schema(
         videoId: { type: String, require: true },
         level: { type: String },
     },
-    { timestamps: true },
+    {
+        _id: false,
+        timestamps: true,
+    },
 )
 
-// add plugins
+// plugin slug
 mongoose.plugin(slug)
+
+// plugin auto tăng giá trị id (vấn đề căng khi nhiều user click vào cùng 1 button (vd: thêm course) cùng 1 thời điểm)
+Course.plugin(AutoIncrement)
+
+// plugin dành cho các thao tác delete,... và override all thao tác
 Course.plugin(mongooseDelete, {
     deletedAt: true,
     overrideMethods: 'all',
